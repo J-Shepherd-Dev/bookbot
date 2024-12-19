5c131f61ecac732ab62f4fe2e49883b82610a078
J-Shepherd-Dev <shepstar141@gmail.com>
2024-12-19 19:38:33 +0000

    Added report

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-def countChar(file_contents):
+def sort_on(dict):
+    return dict["num"]
+def countChar(file_contents,wordcount):
     letterdictonary = {}
+    letter_list = []
     wordslowercase = file_contents.lower()
     for char in wordslowercase:
-        if char not in letterdictonary:
-            letterdictonary[char] = 1
-        else:
-            letterdictonary[char] += 1
-    print(letterdictonary)
+        if char.isalpha():
+            if char not in letterdictonary:
+                letterdictonary[char] = 1
+            else:
+                letterdictonary[char] += 1
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{wordcount} words found in the document\n")
+    for char in letterdictonary:
+        if char.isalpha():
+            letter_list.append({"char": char, "num": letterdictonary[char]})
+    letter_list.sort(reverse=True, key=sort_on)
+    for item in letter_list:
+        print(f"The '{item['char']}' character was found {item['num']} times")
+    print("--- End report ---")
 
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
         wordcount = len(file_contents.split())
-        print(wordcount)
-        countChar(file_contents)
+        countChar(file_contents,wordcount)
 
 
 main()
